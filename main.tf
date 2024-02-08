209fe54af16935120d535944d0d482e42e8f3e85
Mohamed Faizal <mofaizal@microsoft.com>
2024-02-08 11:21:24 +0800

    Initial code

---
+++ main.tf
@@ -0,0 +1,81 @@
+
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0, < 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0, < 4.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+  suffix  = ["afun"]
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "azure-functions-example-rg"
+  location = "South East Asia"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "${module.naming.storage_account.name_unique}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "${module.naming.app_service_plan.name_unique}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      kind
+    ]
+  }
+}
+
+
+resource "azurerm_function_app" "example" {
+  name                       = "${module.naming.function_app.name_unique}"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  os_type                    = "linux"
+  version                    = "~4"
+
+    site_config {
+ linux_fx_version = "python|3.9"
+    }
+    app_settings = {
+FUNCTIONS_WORKER_RUNTIME = "python"
+    }
+  }
+
+
+
